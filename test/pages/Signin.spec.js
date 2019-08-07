13dd0a711b3c2edcfd0e5fb3b0b5f924ffb76db3
Dimitri Pommerole <dpommerole@atecna.fr>
2019-08-07 13:49:05 +0200

    test: add first unit test

---
+++ test/pages/Signin.spec.js
@@ -0,0 +1,30 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Signin from '@/pages/signin.vue'
+import Vuelidate from 'vuelidate'
+
+let wrapper = null
+
+const localVue = createLocalVue()
+localVue.use(Vuelidate)
+
+beforeEach(() => {
+  // Given
+  wrapper = shallowMount(Signin, {
+    localVue,
+    stubs: {
+      'b-form': true,
+      'b-form-input': true,
+      'b-form-group': true,
+      'b-form-invalid-feedback': true,
+      'nuxt-link': true,
+      'b-button': true
+    }
+  })
+})
+
+describe('Signin page', () => {
+  it('Should mount properly', () => {
+    expect(wrapper.exists()).toBe(true)
+  })
+})
+
