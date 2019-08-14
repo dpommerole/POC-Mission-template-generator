d61abbb0988daf24729ac54fc695f70409266019
Dimitri Pommerole <dpommerole@atecna.fr>
2019-08-14 11:06:01 +0200

    test:add structure for index test

---
+++ test/e2e/integration/index.spec.js
@@ -0,0 +1,14 @@
+describe('Index test page', () => {
+  it('Should login and redirect', () => {
+    // Create a fixture which will mock the backend request
+    // type in the field, and click on the submit button
+  })
+
+  it('Should show an error if a if isn‘t correctly filled', () => {
+    // type a wrongs infos in the fields and check the visibility of the error message
+  })
+
+  it('Should show an error if there is something wrong with the backend request', () => {
+    // here, we will need a fixture to mock the backend request , with an error this time
+  })
+})
